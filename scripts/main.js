4094f2b72b96252b14642861f3125b454cdc535a
Tim <tjk315@gmail.com>
2018-12-30 01:09:55 -0500

    Edit adaptive layout with improved CSS visual effects

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,10 @@
 const DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 const DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+const DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
 const THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+const HIDDEN_DETAIL_CLASS = 'hidden-detail';
+const TINY_EFFECT_CLASS = 'is-tiny';
+const ESC_KEY = 27;
 
 function setDetails(imageUrl, titleText) {
   'use strict';
@@ -32,8 +36,8 @@ function addThumbClickHandler(thumb) {
   thumb.addEventListener('click', function (e) {
     e.preventDefault();
     setDetailFromThumb(thumb);
+    showDetails();
   });
-// Add last two functions in here?
 }
 
 function getThumbnailsArray() {
@@ -44,10 +48,37 @@ function getThumbnailsArray() {
   return thumbnailArray;
 }
 
+function hideDetails() {
+  'use strict';
+  document.body.classList.add(HIDDEN_DETAIL_CLASS);
+}
+
+function showDetails() {
+  'use strict';
+  const frame = document.querySelector(DETAIL_FRAME_SELECTOR);
+  document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+  frame.classList.add(TINY_EFFECT_CLASS);
+  setTimeout(function () {
+    frame.classList.remove(TINY_EFFECT_CLASS);
+  }, 50);
+}
+
+function addKeyPressHandler() {
+  'use strict'
+  document.body.addEventListener('keyup', function(e) {
+    e.preventDefault();
+    console.log(e.keyCode);
+    if (e.keyCode === ESC_KEY) {
+      hideDetails();
+    }
+  });
+}
+
 function initializeEvents() {
   'use strict';
   const thumbnails = getThumbnailsArray();
   thumbnails.forEach(addThumbClickHandler);
+  addKeyPressHandler();
 }
 
 initializeEvents();
